1d4db8832aae32600544daebfa3afc1e348304d7
whome000 <160709028+whome000@users.noreply.github.com>
2025-02-26 19:15:27 +0900

    added bugzilla

--- hana.js
+++ hana.js
@@ -6,22 +6,32 @@ chrome.contextMenus.onClicked.addListener(genericOnClick);
 // A generic onclick callback function.
 function genericOnClick(info) {
       sel = info.selectionText;
-      newURL = "https://jira.tools.sap/browse/" + sel;
-      console.log(newURL);
-      chrome.tabs.create({ url: newURL });
+      switch (info.menuItemId) {
+            case 'jira':
+                  newURL = "https://jira.tools.sap/browse/" + sel;
+                  console.log(newURL);
+                  chrome.tabs.create({ url: newURL });
+                  break;
+            case 'bugzilla':
+                  newURL = "https://hdbits.wdf.sap.corp/bugzilla/show_bug.cgi?id=" + sel;
+                  console.log(newURL);
+                  chrome.tabs.create({ url: newURL });
+                  break;
+      }
 }
 chrome.runtime.onInstalled.addListener(function () {
-  // Create one test item for each context type.
-  let contexts = [
-    'selection'
-  ];
-  for (let i = 0; i < contexts.length; i++) {
-    let context = contexts[i];
+
     let title = "Open HANA jira";
     chrome.contextMenus.create({
       title: title,
-      contexts: [context],
-      id: context
+      contexts: 'selection',
+      id: 'jira'
     });
+    let title = "Open Bugzilla";
+    chrome.contextMenus.create({
+      title: title,
+      contexts: 'selection',
+      id: 'bugzilla'
+    });      
   }
 });
